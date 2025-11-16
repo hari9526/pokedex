e79cb2b034bd391cd79db1f612ac0da3bd43c9b1
Harikrishnan R <hari04.krishnan@gmail.com>
2025-11-16 19:56:14 +0530

    Integrate get api

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { Pokemon } from "./pokemon";
 
 export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]); 
+
+  useEffect( () => {
+    // fetch pokemon data from pokeapi.co
+    fetchPokemns();
+  }, []);  
+
+  async function fetchPokemns(){
+    try{
+      const response =  await fetch("https://pokeapi.co/api/v2/pokemon?limit=20"); 
+      const data = await response.json();
+      
+      setPokemons(data.results); 
+      console.log(pokemons);
+    }
+    catch(error){
+      console.error("Error fetching Pokemon data:", error);
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+      { pokemons.map( (pokemon) => (
+        <View key={pokemon.name}>
+          <Text>{pokemon.name}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
